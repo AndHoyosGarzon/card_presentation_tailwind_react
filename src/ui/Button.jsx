0164b23edd1ad0fcf5ed875bf3575e5fb6e5e4f3
AndreHG <andreshg9203@gmail.com>
2025-02-16 23:19:45 -0300

    update styles

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,7 +1,9 @@
 const Button = ({ network }) => {
   return (
     <a
-      className="bg-Grey-700  h-[35px] text-White px-16 font-semibold font-Inter text-xs flex items-center justify-center rounded-md hover:brightness-90 transition-all"
+      className="bg-Grey-700  h-[35px] text-White px-16 font-semibold font-Inter text-xs flex items-center justify-center rounded-md hover:bg-Green
+      hover:text-Grey-900
+      transition-all"
       href="#"
     >
       {network}
